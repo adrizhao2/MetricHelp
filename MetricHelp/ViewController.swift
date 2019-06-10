0167ebb6cc25c03a5a941e2d1e71a92f3c7dd540
Adrianna Zhao <zhaocx@bc.edu>
2019-06-10 09:26:13 -0400

    class 1 ex complete

--- MetricHelp/ViewController.swift
+++ MetricHelp/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var distanceField: UITextField!
+    
+    @IBOutlet weak var distanceLabel: UILabel!
+    
+    @IBOutlet weak var useMetricSwitch: UISwitch!
+    
+    @IBOutlet weak var resultsLabel: UILabel!
+    
+    let milesToKm = 1.60934
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func toggleMetricSwitch(_ sender: Any) {
+        if useMetricSwitch.isOn {
+            distanceLabel.text = "miles"
+        } else {
+            distanceLabel.text = "kilometers"
+        }
+        resultsLabel.text = ""
+    }    
+    @IBAction func convertButtonPressed(_ sender: UIButton) {
+        if let distanceRun = Double(distanceField.text!) {
+            if useMetricSwitch.isOn {
+                let result = distanceRun * milesToKm
+                resultsLabel.text = "You ran \(distanceRun) miles. That's \(result) kilometers!"
+            } else {
+                let result = distanceRun / milesToKm
+                resultsLabel.text = "You ran \(distanceRun) kilometers. That's \(result) miles!"
+            }
+        } else {
+            // I couldn't convert to a Double. I got a nil.
+            resultsLabel.text = "Please enter a valid number without any letters, spaces, or symbols."
+        }
+    }
 }
-
